6c7c4740c1997be207058596430ce40f54d0ef85
Administrator <miguel.casado@gmail.com>
2017-04-17 18:06:30 +0200

    Change to external variables the InitialDelay & Frequency of startup module

--- app/service/SchedulingStartUp.java
+++ app/service/SchedulingStartUp.java
@@ -11,16 +11,27 @@ import play.*;
 import scala.concurrent.duration.Duration;
 
 public class SchedulingStartUp  {
+	   
+	  Configuration configuration;
 
 		@Inject
-	    public SchedulingStartUp(final ActorSystem system,
+	    public SchedulingStartUp(Configuration configuration, final ActorSystem system,
 	                          @Named("update-db-crawler") ActorRef updateDbActor) {
+			
+			int initialDelay=Integer.parseInt(configuration.getString("SchedulingStartUp.InitialDelay"));
+			int frequency = Integer.parseInt(configuration.getString("SchedulingStartUp.Frequency"));
 			
 			
 			play.Logger.debug("SchedulingStartUp");
 	        system.scheduler().schedule(
-	            Duration.create(30, TimeUnit.SECONDS), //Initial delay
-	            Duration.create(1, TimeUnit.MINUTES),     //Frequency
+	            //Duration.create(30, TimeUnit.SECONDS), //Initial delay
+	            //Duration.create(1, TimeUnit.MINUTES),     //Frequency
+
+	        		
+	        		
+	        	Duration.create(initialDelay, TimeUnit.SECONDS), //Initial delay
+		        Duration.create(frequency, TimeUnit.MINUTES),     //Frequency
+		            	
 	            updateDbActor,
 	            "update",
 	            system.dispatcher(),
